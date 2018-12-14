4069256a36a4ef1a0f174f17629db549d3a1fd9e
albert <albert@wifi-roaming-128-4-123-80.host.udel.edu>
2018-12-13 23:24:59 -0500

    Finally!!!

--- FinalExamProject/src/test/java/pkgEmpty/RetirementTest.java
+++ FinalExamProject/src/test/java/pkgEmpty/RetirementTest.java
@@ -4,11 +4,20 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import pkgCore.Retirement;
+
 public class RetirementTest {
 
 	@Test
-	public void TestPMT() {
-		fail("Not yet implemented");
+	public void TestTotalAmountSaved() {
+		Retirement r = new Retirement(40,0.07,20,0.02,10000.0,2642.0);
+		assertEquals(r.TotalAmountSaved(),1454485.55,0.01);
+	}
+	
+	@Test
+	public void TestAmountToSave() {
+		Retirement r = new Retirement(40,0.07,20,0.02,10000.0,2642.0);
+		assertEquals(r.AmountToSave(),554.13,0.01);
 	}
 
 }
